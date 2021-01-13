5854fd47cf80b3d8ce85364a50e1986acc81c4fd
xuchengpu <1550540124@qq.com>
2021-01-13 17:13:30 +0800

    添加dokit混淆，修复不显示dokit工具类界面问题

--- app/src/main/java/com/cainiao/app/MainActivity.kt
+++ app/src/main/java/com/cainiao/app/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.didichuxing.doraemonkit.DoraemonKit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        DoraemonKit.install(application, mutableListOf(),"cainiao");
 //        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 //
 //        val navController = findNavController(R.id.nav_host_fragment)
