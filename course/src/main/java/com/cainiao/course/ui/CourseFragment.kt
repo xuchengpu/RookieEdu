d6f35e1269c4a296612454f56ec9a916a1227b78
xuchengpu <1550540124@qq.com>
2021-01-28 19:39:24 +0800

    实现课程列表popwindow

--- course/src/main/java/com/cainiao/course/ui/CourseFragment.kt
+++ course/src/main/java/com/cainiao/course/ui/CourseFragment.kt
@@ -1,14 +1,20 @@
 package com.cainiao.course.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.databinding.ObservableInt
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
+import androidx.viewbinding.ViewBinding
 import com.blankj.utilcode.util.ToastUtils
 import com.cainiao.common.base.BaseFragment
 import com.cainiao.course.R
 import com.cainiao.course.databinding.FragmentCourseBinding
+import com.cainiao.course.databinding.PopCourseTypeBinding
 import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -21,9 +27,12 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
  */
 class CourseFragment : BaseFragment() {
     private val viewModel: CourseViewModel by viewModel()
+    private lateinit var mBinding: FragmentCourseBinding
+    private var code = "all"
     override fun bindView(view: View, savedInstanceState: Bundle?): ViewDataBinding? {
-        return FragmentCourseBinding.bind(view).apply {
-            val adapter=viewModel.courseAdapter
+        mBinding = FragmentCourseBinding.bind(view)
+        return mBinding.apply {
+            val adapter = viewModel.courseAdapter
             rvCourse.adapter = adapter.withLoadStateFooter(CourseLoadAdapter(adapter))
 
             viewModel.apply {
@@ -52,37 +61,24 @@ class CourseFragment : BaseFragment() {
                         val count = tlCategoryCourse.tabCount
 
                         val index = tab?.position ?: 0
-                        val code = if (index > 0) {
-                            courseTypes?.get(index)?.code ?: "all"
+                        code = if (index > 0) {
+                            courseTypes?.get(index - 1)?.code ?: "all"
                         } else "all"
-
-                        lifecycleScope.launchWhenCreated {
-                            viewModel.apply {
-                                getCourseList(code = code).collect {
-                                    adapter.submitData(it)
-                                    rvCourse.scrollToPosition(0)
-                                }
-                            }
-                        }
-
+                        refreshData(code)
                     }
 
-                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
-                    }
-
-                    override fun onTabReselected(tab: TabLayout.Tab?) {
-
-                    }
+                    override fun onTabReselected(tab: TabLayout.Tab?) {}
 
                 })
 
             }
-            adapter.addLoadStateListener { loadState->
-                if(loadState.refresh is LoadState.Loading){
-                    pbFragmentCourse.visibility=View.VISIBLE
-                }else{
-                    pbFragmentCourse.visibility=View.GONE
+            adapter.addLoadStateListener { loadState ->
+                if (loadState.refresh is LoadState.Loading) {
+                    pbFragmentCourse.visibility = View.VISIBLE
+                } else {
+                    pbFragmentCourse.visibility = View.GONE
                 }
                 // getting the error
                 val error = when {
@@ -96,6 +92,20 @@ class CourseFragment : BaseFragment() {
                 }
 
             }
+            spinnerTypeCourse.setOnClickListener {
+                popWindow.showAsDropDown(it)
+            }
+        }
+    }
+
+    private fun refreshData(code: String, courseType: Int = -1) {
+        lifecycleScope.launchWhenCreated {
+            viewModel.apply {
+                getCourseList(code = code, course_type = courseType).collect {
+                    viewModel.courseAdapter.submitData(it)
+                    mBinding.rvCourse.scrollToPosition(0)
+                }
+            }
         }
     }
 
@@ -106,7 +116,41 @@ class CourseFragment : BaseFragment() {
     override fun initConfig() {
         super.initConfig()
         viewModel.getCourseCategory()
+        configPopFilter()
+    }
+
+    private lateinit var popWindow: PopupWindow
+    private fun configPopFilter() {
+        val popBinding =
+            PopCourseTypeBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+        popWindow = PopupWindow(
+            popBinding.root,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        val obCheckPos = ObservableInt(0)
+        popBinding.apply {
+            pos = obCheckPos
+            tvAllType.setOnClickListener {
+                obCheckPos.set(0)
+                popWindow.dismiss()
+                refreshData(code)
+            }
+            tvBizType.setOnClickListener {
+                obCheckPos.set(1)
+                popWindow.dismiss()
+                refreshData(code, 1)
+            }
+            tvProType.setOnClickListener {
+                obCheckPos.set(2)
+                popWindow.dismiss()
+                refreshData(code, 2)
+            }
+
+
+        }
 
 
     }
+
 }
